186775121d5f4e0fce2d086f95b1b551dd5b351c
MisterE581 <elmyrza581@gmail.com>
2026-05-11 14:49:19 +0600

    v3

--- mysite/store_app/admin.py
+++ mysite/store_app/admin.py
@@ -22,12 +22,10 @@ class ProductAdmin(TranslationAdmin):
 
     class Media:
         js = (
-            'admin/js/jquery.init.js',
-            'js/admin/custom.js',
+            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
+            'modeltranslation/js/tabbed_translation_fields.js',
         )
-
         css = {
-            'all': (
-                'css/admin/custom.css',
-            )
+            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
         }
